85277da4d3f4fbda7186db447638dbfaca908663
Scott Willsey <scott@saw66.com>
2022-11-05 01:59:43 -0700

    Initial npm and astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://sierraalfawhiskey.com/",
+  trailingSlash: "ignore",
+  outDir: "dist",
+  integrations: [sitemap(), image(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
