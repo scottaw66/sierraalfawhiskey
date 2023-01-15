538045c4f3017e1cbb65177945aac40d7f7a7608
Scott Willsey <sunny.flame0794@fastmail.com>
2023-01-15 10:55:38 -0800

    HEY LET'S TRY THE ASTRO 2 BETA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import remarkGfm from "remark-gfm";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -14,12 +12,7 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
-    mdx(),
   ],
-  markdown: {
-    remarkPlugins: [remarkGfm],
-    extendDefaultPlugins: true,
-  },
   vite: {
     ssr: {
       external: ["svgo"],
