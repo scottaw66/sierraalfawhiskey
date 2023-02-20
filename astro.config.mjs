eec55e301233102879295b0070f179615d14c396
Scott Willsey <sunny.flame0794@fastmail.com>
2023-02-20 00:32:23 -0800

    Demos broken image import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
-import embeds from "astro-embed/integration";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    contentCollections: true,
+  },
   site: "https://sierraalfawhiskey.com/",
   trailingSlash: "ignore",
   outDir: "dist",
   integrations: [
-    embeds(),
     sitemap(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
