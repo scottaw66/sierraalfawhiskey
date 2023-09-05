1efe65e523a645b969ec8918e97db6b87a455cfa
Scott Willsey <sunny.flame0794@fastmail.com>
2023-09-05 11:48:07 -0700

    Fixes npm build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,5 @@
 import { defineConfig } from "astro/config";
-import pagefind from "astro-pagefind";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
-  site: "https://sierraalfawhiskey.com/",
-  outDir: "dist",
-  integrations: [pagefind(), sitemap()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
 });
